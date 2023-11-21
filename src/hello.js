e39195a2cee5107aefd5855a1f4296a29d0f24a8
kekez <zhang.kek@northeastern.edu>
2023-11-20 19:29:43 -0500

    optimize structure to keep backend files

--- src/hello.js
+++ src/hello.js
@@ -1,7 +1,10 @@
 function HelloRoutes(app) {
-    const hello = (req, res) => {
+    app.get("/hello", (req, res) => {
         res.send("Life is good!");
-    };
-    app.get("/hello", hello);
+    });
+    app.get('/', (req, res) => {
+        res.send('Welcome to Full Stack Development!')
+    })
 }
+
 export default HelloRoutes;
